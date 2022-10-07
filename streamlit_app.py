6748dad10de6c6ebf1bc923d6e946c1f01d6a2a4
PRIYADE20 <115225199+PRIYADE20@users.noreply.github.com>
2022-10-07 19:03:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie  🥗 ')
 streamlit.text('Hard-Boiled Free-Range Egg 🐔')
  
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
