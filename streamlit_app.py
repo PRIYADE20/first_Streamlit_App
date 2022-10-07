220ede907dbaadf429706436bf5c196ff34319ef
PRIYADE20 <115225199+PRIYADE20@users.noreply.github.com>
2022-10-07 19:02:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
-streamlit.title("New Learnings")
-streamlit.header('Breakfast Menu')
+streamlit.title("Being Foodie  🥑🍞")
+streamlit.header('Breakfast Menu 🥣')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Kale, Spinach & Rocket Smoothie  🥗 ')
+streamlit.text('Hard-Boiled Free-Range Egg 🐔')
+ 
